ef9a8bac544189c937893bc1d69ccb846f62bc34
Arve Hansen <arveh4@outlook.com>
2022-07-09 12:11:16 +0200

    Renamed RazorPages project

--- AspNet.RazorPages.Net6/Pages/Index.cshtml.cs
+++ AspNet.RazorPages.Net6/Pages/Index.cshtml.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AH.TestAuth.RazorPages.Pages
+namespace AspNet.RazorPages.Net6.Pages
 {
     public class IndexModel : PageModel
     {
