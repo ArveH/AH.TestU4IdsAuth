70f4dd1b0c462503d06a7310219d8d89937d4a07
Arve Hansen <arveh4@outlook.com>
2022-07-09 12:15:40 +0200

    File scoped namespace

--- AspNet.RazorPages.Net6/Pages/Privacy.cshtml.cs
+++ AspNet.RazorPages.Net6/Pages/Privacy.cshtml.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AspNet.RazorPages.Net6.Pages
+namespace AspNet.RazorPages.Net6.Pages;
+
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        _logger = logger;
+    }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
