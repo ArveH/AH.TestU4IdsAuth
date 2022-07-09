75b1c1a3aeab1e22bd02fadebb63baee8e936493
Arve Hansen <arveh4@outlook.com>
2022-07-09 11:41:49 +0200

    Renamed Solution

---
+++ AH.TestU4IdsAuth.RazorPages/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = builder.Configuration["OpenIdConnect:Authority"];
+        options.ClientId = builder.Configuration["OpenIdConnect:ClientId"];
+        options.ClientSecret = ""; // Not needed for PKCE
+
+        options.ResponseType = "code"; // Authorization code
+        options.UsePkce = true; // It's default true, so don't really need this
+        options.SaveTokens = true;
+    });
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
